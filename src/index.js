9de9db1a91329a8cd697cf923040d4673d05a9d5
Dzmitry Yatsyna <dyfromby@gmail.com>
2020-03-01 23:18:22 +0300

    feat: add min/max/avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
 exports.min = function min (array) {
+if (!array || array.length == 0) {
   return 0;
+}
+else {
+  return array.sort((a, b) => a - b)[0]
+}
 }
 
 exports.max = function max (array) {
+if (!array || array.length == 0) {
   return 0;
+}
+else {
+  return array.sort((a, b) => a - b)[array.length-1]
+}
 }
 
 exports.avg = function avg (array) {
+if ( !array || array.length == 0) {
   return 0;
+}
+else {
+  let s=0;
+  array.forEach(i => s += i)
+  return s/array.length
+}
 }
